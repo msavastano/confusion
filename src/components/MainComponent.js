132e97af09372910a6fdda6e4987701551536a26
msavast1@its.jnj.com <msavast1@its.jnj.com>
2019-01-04 20:12:15 -0500

    Assignment2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
 
 class Main extends Component {
   constructor(props) {
@@ -52,6 +53,7 @@ class Main extends Component {
         */}
         <Switch>
             <Route path="/home" component={HomePage} />
+            <Route path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
             <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path='/contactus' component={Contact} />} />
---
+++ src/components/RenderLeaderComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Media } from 'reactstrap';
+
+function RenderLeader ({leader}) {
+    return (
+        
+        <Media tag="li">
+            <Media left middle>
+                <Media object src={leader.image} alt={leader.name} />
+            </Media>
+            <Media body className="ml-5">
+            <Media heading>{leader.name}</Media>
+            <p>{leader.designation}</p>
+            <p>{leader.description}</p>
+        </Media>
+    </Media>
+    );
+}
+
+export default RenderLeader;
